b0a9510c3153fd6069f0f9431c38c925c28c7229
Pratik-Mahajan257 <79212698+Pratik-Mahajan257@users.noreply.github.com>
2023-04-19 15:24:24 +0530

    first commit

---
+++ client/src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+import LatestProducts from '../components/LatestProducts.js'
+import Hero from '../components/Hero.js';
+
+const Home = () => {
+
+  
+  
+
+  return (
+    <section>
+    <Hero />
+    <LatestProducts />
+    </section>
+  )
+};
+
+export default Home;
